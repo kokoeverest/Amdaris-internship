62e27b616ba94597b68a48409676148971266eda
Kaloyan Peychev <kokoeverest@gmail.com>
2024-03-12 12:04:14 +0200

    Add the latest changes in the solution

--- GitAndVcs/GitAndVcs/Car.cs
+++ GitAndVcs/GitAndVcs/Car.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GitAndVcs
 {
-    internal class Car
+    class Car
     {
-        string color = "Red";
-
+        string brand = string.Empty;
+        string model = string.Empty;
+        public Car(string brandName, string modelName)
+        {
+            brand = brandName;
+            model = modelName;
+        }
+    static void Main(string[] args)
+    {
+        Car car = new Car("Ford", "Mustang");
+        Console.WriteLine(car.model);
+    }
     }
 }
+

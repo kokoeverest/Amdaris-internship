a03dbac76c7f33669c64ccb8912fece0b248ee0f
Kaloyan Peychev <kokoeverest@gmail.com>
2024-03-18 12:29:17 +0200

    Minor change in the program

--- GitAndVcs/GitAndVcs/Program.cs
+++ GitAndVcs/GitAndVcs/Program.cs
@@ -1,2 +1,12 @@
-﻿// A test repo for the Git and VCS assessment
-//Console.WriteLine("Hello Amdaris!");
+﻿using System.Text;
+
+StringBuilder builder = new("", 200);
+
+builder.AppendFormat("{0, -20} - {1, 15:C}", DateTime.Now.DayOfYear, 2000);
+
+Console.WriteLine(builder);
+
+DateTime dateTime = new(year: 2022, month: 3, day: 20);
+TimeSpan duration = new(days: 36, 0,0,0);
+Console.WriteLine(dateTime);
+Console.WriteLine(dateTime.Add(duration));

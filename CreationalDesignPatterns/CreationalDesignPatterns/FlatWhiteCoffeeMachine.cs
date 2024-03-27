455f6200792ef3fe93175abbe9e8cf5644db808d
Kaloyan Peychev <kokoeverest@gmail.com>
2024-03-27 11:25:00 +0200

    Fix a typo

--- CreationalDesignPatterns/CreationalDesignPatterns/FlatWhiteCoffeeMachine.cs
+++ CreationalDesignPatterns/CreationalDesignPatterns/FlatWhiteCoffeeMachine.cs
@@ -6,7 +6,7 @@
 
         public override string ToString()
         {
-            return $"Flat White machine prepares {Coffee} {base.Coffee} + {base.Milk}";
+            return $"Flat White machine prepares {Coffee} + {base.Coffee} + {base.Milk}";
         }
     }
 }
